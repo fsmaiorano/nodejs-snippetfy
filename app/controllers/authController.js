eda9954d4100c981505431ee61c17a426d72d008
fabio.maiorano <fabio.maiorano@D-CB-001330>
2018-04-25 07:37:33 -0300

    add: register

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -1,8 +1,23 @@
+const { User } = require('../models');
+const bcrypt = require('bcryptjs');
+
 module.exports = {
   signin(req, res) {
     return res.render('auth/signin');
   },
   signup(req, res) {
     return res.render('auth/signup');
+  },
+  async register(req, res) {
+    const { email } = req.body;
+    if (await User.findOne({ where: { email } })) {
+      return res.redirect('back');
+    }
+
+    const password = await bcrypt.hash(req.body.password, 5);
+
+    // { name, email, password, password}
+    await User.create({ ...req.body, password });
+    return res.redirect('/');
   },
 };
