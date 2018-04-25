575494f5ce9c31cc2bdddeca38e99bb9b3fe7e6d
fabio.maiorano <fabio.maiorano@D-CB-001330>
2018-04-25 14:46:30 -0300

    add: create categories and snippets migrations

---
+++ database/migrations/20180425152758-create-categories.js
@@ -0,0 +1,35 @@
+module.exports = {
+  up: (queryInterface, DataTypes) => {
+    queryInterface.createTable('Categories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      UserId: {
+        type: DataTypes.INTEGER,
+        references: { model: 'Users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        allowNull: false,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    });
+  },
+
+  down: (queryInterface) => {
+    queryInterface.dropTable('Categories');
+  },
+};
