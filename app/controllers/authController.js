1a064bf43f7f28913946fe204e925423e7a593ff
fabio.maiorano <fabio.maiorano@D-CB-001330>
2018-04-25 12:24:38 -0300

    add: new pages and middlewares

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -2,25 +2,62 @@ const { User } = require('../models');
 const bcrypt = require('bcryptjs');
 
 module.exports = {
+
   signin(req, res) {
     return res.render('auth/signin');
   },
+
   signup(req, res) {
     return res.render('auth/signup');
   },
-  async register(req, res) {
-    const { email } = req.body;
-    if (await User.findOne({ where: { email } })) {
-      req.flash('error', 'E-mail já cadastrado');
-      return res.redirect('back');
+
+  async register(req, res, next) {
+    try {
+      const { email } = req.body;
+      if (await User.findOne({ where: { email } })) {
+        req.flash('error', 'E-mail já cadastrado');
+        return res.redirect('back');
+      }
+
+      const password = await bcrypt.hash(req.body.password, 5);
+
+      // { name, email, password, password}
+      await User.create({ ...req.body, password });
+
+      req.flash('success', 'Usuário cadastrado com sucesso');
+      return res.redirect('/');
+    } catch (err) {
+      return next(err);
     }
+  },
 
-    const password = await bcrypt.hash(req.body.password, 5);
+  async authenticate(req, res, next) {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ where: { email } });
 
-    // { name, email, password, password}
-    await User.create({ ...req.body, password });
+      if (!user) {
+        req.flash('error', 'Usuário inexistente');
+        return req.redirect('back');
+      }
 
-    req.flash('success', 'Usuário cadastrado com sucesso');
-    return res.redirect('/');
+      if (!await bcrypt.compare(password, user.password)) {
+        req.flash('error', 'Senha incorreta');
+        return res.redirect('back');
+      }
+
+      req.session.user = user;
+      return req.session.save(() => {
+        res.redirect('app/dashboard');
+      });
+    } catch (err) {
+      return next(err);
+    }
+  },
+
+  signout(req, res) {
+    return req.session.destroy(() => {
+      res.redirect('/');
+    });
   },
 };
